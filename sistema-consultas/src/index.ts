3bdf91d99d406072e5e2c2766ab88909026988fc
hrqmartins <hrqmartins129@gmail.com>
2026-02-25 20:19:38 -0300

    feat: Função para criar consulta.

--- sistema-consultas/src/index.ts
+++ sistema-consultas/src/index.ts
@@ -6,18 +6,35 @@ import { Consulta } from "./interfaces/consulta";
 
 const cardiologia: Especialidade = {
   id: 1,
-  nome: "Cardiologia",
+  nome: "Cardiologista",
 };
 const medico1: Medico = {
   id: 1,
-  nome: "Dr. Roberto Silva",
-  crm: "CRM12345",
+  nome: "Dr. Paulo Muzy",
+  crm: "CRM654321",
   especialidade: cardiologia,
   ativo: true,
 };
 const paciente1: Paciente = {
   id: 1,
-  nome: "Carlos Andrade",
+  nome: "Guilherme Macedo",
   cpf: "123.456.789-00",
-  email: "carlos@email.com",
+  email: "guilhermemacedo@email.com",
 };
+
+function criarConsulta(
+  id: number,
+  medico: Medico,
+  paciente: Paciente,
+  data: Date,
+  valor: number
+): Consulta {
+  return {
+    id,
+    medico,
+    paciente,
+    data,
+    valor,
+    status: "Agendada",
+  };
+}
